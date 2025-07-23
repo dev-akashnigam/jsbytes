92bdd014b2447d4cf3b21020f1c0566db4b318ae
Akash Nigam <contact.akashnigam@gmail.com>
2025-07-23 14:14:24 +0530

    Adding program showing the use of single line and multi line comment

---
+++ 011-comments.js
@@ -0,0 +1,13 @@
+let city = "Bangalore";  // setting city as 'Bangalore' (this is a single line comment)
+
+console.log(`The city that helped me establish my existence in India : ${city}`);
+
+/*
+This is multi line comment, same as java.
+I will update city to 'Seattle' in the code below and then print it.
+*/
+
+city = "Seattle";
+
+console.log(`The city that I loved living in outside India and wish to be there again is : ${city}`);
+
