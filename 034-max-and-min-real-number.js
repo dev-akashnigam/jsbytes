eed7f49af4df90fca2e125a5024b40f6d87409ab
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-01 15:35:49 +0530

    Max and Min real and decimal numbers supported by JS

---
+++ 034-max-and-min-real-number.js
@@ -0,0 +1,5 @@
+const maxRealNo = Number.MAX_SAFE_INTEGER;
+const minRealNo = -Number.MAX_SAFE_INTEGER-1;
+
+console.log(`Max real number supported by JS = ${maxRealNo}`);
+console.log(`Min real number supported by JS = ${minRealNo}`);
---
+++ 035-max-and-min-decimal-number.js
@@ -0,0 +1,5 @@
+const maxDecimalNo = Number.MAX_VALUE;
+const minDecimalNo = -Number.MAX_VALUE-1;
+
+console.log(`Max decimal number supported by JS = ${maxDecimalNo}`);
+console.log(`Min decimal number supported by JS = ${minDecimalNo}`);
