0d07acd0bcaee3bead578541d3f527ba4c0623b5
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-01 16:00:42 +0530

    Positive and Negative Infinity in JS

---
+++ 036-positive-and-negative-infinity.js
@@ -0,0 +1,5 @@
+const positiveInfinity = Infinity;
+const negativeInfinity = -Infinity;
+
+console.log(`Positive infinity in JS = ${positiveInfinity}`)
+console.log(`Negative infinity in JS = ${negativeInfinity}`)
