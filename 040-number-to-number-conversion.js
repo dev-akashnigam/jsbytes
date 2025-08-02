16dd8f460f671fb0ebd50aafaa733864a56eafd5
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-02 15:54:59 +0530

    Number to Number interconversion and text/string to boolean conversion

---
+++ 040-number-to-number-conversion.js
@@ -0,0 +1,8 @@
+const realNo = 78;
+const decimalNo = 89.2145;
+
+const floatConvertedFromInteger = parseFloat(realNo);
+const integerConvertedFromFloat = parseInt(decimalNo);
+
+console.log(`Real Number: ${realNo} converted to decimal number becomes = ${floatConvertedFromInteger}`);
+console.log(`Decimal Number: ${decimalNo} converted to real number becomes = ${integerConvertedFromFloat}`);
---
+++ 041-text-to-boolean-conversion-placeholder.js
@@ -0,0 +1,3 @@
+// there is no direct way to convert a String to Boolean in JS
+// Methods like parseBoolean is not available in JS
+// casting string to boolean (boolean)str also does not work.
