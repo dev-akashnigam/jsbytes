4701a01f386ad4147e305fc0f0ce6e8a59e0e252
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-08 11:32:30 +0530

    Updating parseInt() and parseFloat() to Number.parseInt() and Number.parseFloat()

--- 043-number-to-number-conversion.js
+++ 043-number-to-number-conversion.js
@@ -1,8 +1,8 @@
 const realNo = 78;
 const decimalNo = 89.2145;
 
-const floatConvertedFromInteger = parseFloat(realNo);
-const integerConvertedFromFloat = parseInt(decimalNo);
+const floatConvertedFromInteger = Number.parseFloat(realNo);
+const integerConvertedFromFloat = Number.parseInt(decimalNo);
 
 console.log(`Real Number: ${realNo} converted to decimal number becomes = ${floatConvertedFromInteger}`);
 console.log(`Decimal Number: ${decimalNo} converted to real number becomes = ${integerConvertedFromFloat}`);
--- 050-table-with-all-datatpe-conversions.js
+++ 050-table-with-all-datatpe-conversions.js
@@ -1,4 +1,4 @@
 /*
 Please visit the link:
-https://drive.google.com/file/d/1lVQtPA5016R3cmJa6fbzrDAj9l_IzWhq/view?usp=sharing
+https://drive.google.com/file/d/1r6KoMem9UoemlmtBhAMywswTYPgfOTHC/view?usp=sharing
 */
