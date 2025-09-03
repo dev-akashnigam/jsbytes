56b72fa6b384bcf3f26f797bc5c764d7c7f20cf7
Akash Nigam <contact.akashnigam@gmail.com>
2025-09-03 07:06:05 +0530

    Calculate square using normal function, function expression and arrow function

---
+++ 112-arrow-function-calculate-square.js
@@ -0,0 +1,13 @@
+let getSquare = (num) => {
+    let square = num * num;
+    return square;
+};
+
+const prompt = require('prompt-sync')();
+
+console.log("Please enter an integer: ");
+const myNum = Number(prompt());
+
+const square = getSquare(myNum);
+console.log(`Square of number: ${myNum} = ${square}`);
+
