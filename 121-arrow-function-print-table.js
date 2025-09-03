0ac923f6f3a71a91053fe9a84b1286c133d2e4f4
Akash Nigam <contact.akashnigam@gmail.com>
2025-09-03 13:18:09 +0530

    Print table of a number using regular function, function expression and arrow function

---
+++ 121-arrow-function-print-table.js
@@ -0,0 +1,12 @@
+let printTable = (num) => {
+    for(let i=1; i<=10; i++) {
+        console.log(`${num} X ${i} = ${num*i}`);
+    }
+};
+
+const prompt = require('prompt-sync')();
+
+console.log("Please enter a number: ");
+let myNum = Number(prompt())
+
+printTable(myNum);
