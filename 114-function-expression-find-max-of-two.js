180e519be08469d650f6436f65fa8add927efa14
Akash Nigam <contact.akashnigam@gmail.com>
2025-09-03 11:22:32 +0530

    Finding max of two numbers using normal function, function expression and arrow function

---
+++ 114-function-expression-find-max-of-two.js
@@ -0,0 +1,19 @@
+let getMaxOfTwo = function (a, b) {
+    if(a>=b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+};
+
+const prompt = require('prompt-sync')();
+
+console.log("Please enter first number: ");
+let firstNum = Number(prompt());
+
+console.log("Please enter second number: ");
+let secondNum = Number(prompt());
+
+let largerNumber = getMaxOfTwo(firstNum, secondNum);
+console.log(`Larger amongst ${firstNum} and ${secondNum} = ${largerNumber}`);
